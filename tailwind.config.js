47efd77567367c2e3c6b0ce41c01d5da3e200c10
Leonel Sosa <leo.vm.cba@gmail.com>
2022-04-14 17:13:09 -0300

    The general structure and styles were created

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        liberty: {
+          main: '#485696',
+          shadow_dark: '#242d57',
+          shadow_light: '#9dacec'
+        },
+        pumpkin: '#FC7A1E',
+        goldCrayola: '#F9C784',
+        platinum: '#E7E7E7',
+      },
+      boxShadow: {
+        'shadow-neumorphism': '9px 9px 16px #242d57, -9px -9px 16px #9dacec',
+
+        'btn-shadow': '-6px -6px 18px 0px #9dacec, -4px -4px 4px 0px #9dacec, 6px 6px 18px #0001, 3px 3px 4px 0px #242d57',
+
+        'btn-active': ' inset -5px -5px 16px #9dacec, inset -3px -3px 3px #9dacec, inset 6px 6px 14px #242d57, inset 3px 3px 4px #242d57'
+      }
+    },
+  },
+  plugins: [],
+}
